e218ac1ef865b15ade4f497b65fafc11d168d891
JoaoLuucasOL <joaolucas_deoliveira@yahoo.com.br>
2022-05-25 08:29:57 -0300

    25-05-2022 08-29

---
+++ src/navigations/drawer.navigation.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { ChatScreen, PerfilScreen, MapScreen } from "../screens";
+import colors from "../styles/colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, StyleSheet } from "react-native";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigation() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: { backgroundColor: colors.secondary },
+        headerTintColor: colors.white,
+        drawerStyle: { backgroundColor: colors.secondary },
+        drawerActiveTintColor: colors.white,
+        drawerInactiveTintColor: colors.white,
+      }}
+    >
+      <Drawer.Screen
+        name="Perfil"
+        component={PerfilScreen}
+        options={{
+          drawerIcon: () => (
+            <Ionicons name="person" size={24} color={colors.white} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{
+          drawerIcon: () => (
+            <Ionicons name="chatbubbles" size={24} color={colors.white} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Mapa"
+        component={MapScreen}
+        options={{
+          drawerLabel: "Mapa",
+          drawerIcon: () => (
+            <Ionicons name="map" size={24} color={colors.white} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  title: {
+  color: colors.white,
+  fontWeight: "bold",
+  fontSize: 18,
+  },
+ });
